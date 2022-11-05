0e12d9d070fab6597f20293d84254f78ef843850
Max Base <maxbasecode@gmail.com>
2022-11-05 22:16:07 +0100

    move example from infix.js file and add module.exports

--- Infix2Postfix.js
+++ Infix2Postfix.js
@@ -78,21 +78,4 @@ const convert_infix_to_postfix = (infix) => {
     return postfix;
 };
 
-// Example
-console.log(convert_infix_to_postfix("5 + 6 * 7"));
-// [ '5', '6', '7', '*', '+' ]
-console.log(convert_infix_to_postfix("(((a/b)-c) + (d*e))- (a*c)"));
-// [
-//   'a', 'b', '/', 'c',
-//   '-', 'd', 'e', '*',
-//   '+', 'a', 'c', '*',
-//   '-'
-// ]
-console.log(convert_infix_to_postfix("(k+l)-(m*n)+(o^p)*w/v/u*t+q"));
-// [
-//   'k', 'l', '+', 'm', 'n',
-//   '*', '-', 'o', 'p', '^',
-//   'w', '*', 'v', '/', 'u',
-//   '/', 't', '*', '+', 'q',
-//   '+'
-// ]
+module.exports = convert_infix_to_postfix;
