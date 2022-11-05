faae96296c9d1b5a4143ac8f538eb7945ca5773c
Max Base <MaxBaseCode@Gmail.Com>
2022-11-05 14:09:12 +0100

    Update Infix2Postfix.js

--- Infix2Postfix.js
+++ Infix2Postfix.js
@@ -80,5 +80,26 @@ const convert_infix_to_postfix = (infix) => {
 
 // Example
 console.log(convert_infix_to_postfix("5 + 6 * 7"));
+// [ '5', '6', '7', '*', '+' ]
 console.log(convert_infix_to_postfix("(((a/b)-c) + (d*e))- (a*c)"));
+// [
+//   'a', 'b', '/', 'c',
+//   '-', 'd', 'e', '*',
+//   '+', 'a', 'c', '*',
+//   '-'
+// ]
 console.log(convert_infix_to_postfix("(k+l)-(m*n)+(o^p)*w/v/u*t+q"));
+// [
+//   'k', 'l', '+', 'm', 'n',
+//   '*', '-', 'o', 'p', '^',
+//   'w', '*', 'v', '/', 'u',
+//   '/', 't', '*', '+', 'q',
+//   '+'
+// ]
+console.log(convert_infix_to_postfix("a+b*(c^d-e)^(f+g*h)-i"));
+// [
+//     'a', 'b', 'c', 'd', '^',
+//     'e', '-', 'f', 'g', 'h',
+//     '*', '+', '^', '*', '+',
+//     'i', '-'
+// ]
